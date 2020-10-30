119f9ec6ef4d430dcc773a9ea547d18ad223c7d6
Mike <mike@narx.net>
2020-10-30 21:36:04 +0800

    add remote-debugging-address

--- screenshot.py
+++ screenshot.py
@@ -33,6 +33,7 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--headless")
 options.add_argument("--remote-debugging-port=9222")
+options.add_argument("--remote-debugging-address=0.0.0.0")
 
 #  Check for Crop settings
 if parser.has_section('crop'):
